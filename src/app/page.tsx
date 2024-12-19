9e1516e5ad1fd1f9aa836d158df6ff6547b743f1
Rebecca-J7 <rjenn003@ucr.edu>
2024-12-18 23:29:36 -0800

    Set up files

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/src/components/Header";
+
+const Home = () => {
+  return (
+    <div className="flex w-full flex-col items-center justify-center space-y-16">
+      <div className="m-3" />
+      <Header />
+      <div className="m-10" />
+      <div className="m-20" />
+    </div>
+  );
+};
+
+export default Home;
