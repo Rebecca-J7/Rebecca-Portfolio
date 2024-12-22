5a5fe798dd8d0b54ed5c76e1c4d440f224f815fc
Rebecca-J7 <rjenn003@ucr.edu>
2024-12-21 18:29:29 -0800

    Made introduction file layout and fixed features

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 const Header = () => {
   return (
-    <div className="h-16 w-screen bg-web-purple-1">
+    <div className="fixed top-0 z-50 h-16 w-full bg-web-purple-1">
       <div className="absolute left-7 top-5 flex flex-row gap-5 font-mono text-xl font-semibold">
         <p className="hover text-web-purple-2">rebecca jennings</p>
         <p className="text-black hover:text-web-purple-2">about</p>
---
+++ src/components/Welcome.tsx
@@ -0,0 +1,22 @@
+const Welcome = () => {
+  return (
+    <div className="absolute left-52 top-32 flex flex-col">
+      <div className="flex flex-row gap-4">
+        <p className="font-mono text-4xl font-semibold text-black">
+          Welcome to the
+        </p>
+        <p className="font-mono text-4xl font-semibold text-web-purple-2">
+          Purple Serendipity !
+        </p>
+      </div>
+
+      <p className="relative top-7 max-w-2xl font-mono text-xl font-semibold text-black">
+        you stumble upon this comforting space, perhaps by an occurrence... or a
+        development of events by chance... may it be one of happiness and
+        benefits.
+      </p>
+    </div>
+  );
+};
+
+export default Welcome;
