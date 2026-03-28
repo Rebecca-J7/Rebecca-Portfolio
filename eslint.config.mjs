480a8a8f779f7ba391cb9f77535e2b4cbc80d5e9
Rebecca Jennings <157240153+Rebecca-J7@users.noreply.github.com>
2026-03-28 12:55:40 -0700

    fix packages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,22 +4,23 @@ import tseslint from "typescript-eslint";
 import react from "eslint-plugin-react";
 import prettier from "eslint-plugin-prettier/recommended";
 import query from "@tanstack/eslint-plugin-query";
-import { defineConfig } from 'eslint/config'
-import nextPlugin from '@next/eslint-plugin-next'
-import nextConfig from 'eslint-config-next/core-web-vitals'
+import { defineConfig } from "eslint/config";
+import nextPlugin from "@next/eslint-plugin-next";
+import nextConfig from "eslint-config-next/core-web-vitals";
 
 const eslintConfig = defineConfig([
   {
     ignores: ["dist/*"],
     files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
     ignores: [".next/", "out/", "node_modules"],
-    files: ["**/*.js"], languageOptions: { sourceType: "commonjs" },
+    files: ["**/*.js"],
+    languageOptions: { sourceType: "commonjs" },
     languageOptions: { globals: { ...globals.node } },
     ...eslint.configs.recommended,
     ...tseslint.configs.recommended,
     ...nextConfig,
     plugins: {
-      '@next/next': nextPlugin,
+      "@next/next": nextPlugin,
       react: react,
       prettier: prettier,
       "@typescript-eslint": tseslint.plugin,
@@ -33,8 +34,8 @@ const eslintConfig = defineConfig([
       "@typescript-eslint/no-unused-vars": "error",
     },
   },
-])
-export default eslintConfig
+]);
+export default eslintConfig;
 
 // export default [
 //   {
